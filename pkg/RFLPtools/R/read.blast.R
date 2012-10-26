1345de91279145e1b1e47b662691dee0e42cc51b
stamats <stamats@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-10-26 02:35:31 +0000

    several updates
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rflptools@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RFLPtools/R/read.blast.R
+++ pkg/RFLPtools/R/read.blast.R
@@ -2,13 +2,14 @@
 ## Read BLAST data
 ###############################################################################
 
-read.blast <- function(file){
-    x <- read.delim(file = file,
-                        header = FALSE, stringsAsFactors = FALSE)
+read.blast <- function(file, sep = "\t"){
+    x <- read.table(file = file, header = FALSE, sep = sep, quote = "\"", 
+                    dec = ".", fill = TRUE, comment.char = "", 
+                    stringsAsFactors = FALSE)
     if(ncol(x) != 12)
         stop("Data in given file", basename(file), "has wrong dimension!")
     names(x) <- c("query.id", "subject.id", "identity", "alignment.length",
-                  "missmatches", "gap.opens", "q.start", "q.end", "s.start",
+                  "mismatches", "gap.opens", "q.start", "q.end", "s.start",
                   "s.end", "evalue", "bit.score")
     if(!all(sid <- x[,"subject.id"] %in% x[,"query.id"]))
         warning("The following 'subject.id's do not occur as 'query.id's: ",
